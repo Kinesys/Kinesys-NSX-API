aa05708aeedd886f8031e033c5979030fbdb0fb7
Kinesys <67867993+Kinesys@users.noreply.github.com>
2021-03-01 04:16:10 +0900

    Update Kinesys NSX_MH_API.rb

--- src/Kinesys NSX_MH_API.rb
+++ src/Kinesys NSX_MH_API.rb
@@ -60,3 +60,55 @@ class NsxController
         api("/Iswitch", :post, body)
 
     end
+
+    def delete_logical_switch(lswitch_uuid)
+
+        api("/lswitch/#{lswitch_uuid}", :delete)
+
+    end
+
+    def query_logical_switches()
+
+        api("/lswitch", :get)
+
+    end
+
+    def create_logical_switch_port(lswitch_uuid, body={})
+
+        api("/lswitch/#{lswitch_uuid}/lport", :post, body)
+
+    end
+
+    def delete_logical_switch_port(lswitch_uuid, lport_uuid)
+
+        api("/lswitch/#{lswitch_uuid}/lport/#{lport_uuid}", :delete)
+
+    end
+
+    def update_logical_switch_port_attachment(lswitch_uuid, lport_uuid, body={})
+
+        api("/lswitch/#{lswitch_uuid}/lport#{lport_uuid}/attachment", :put, body)
+
+    end
+
+    def import_switch_control_server_certificate(pem_encoded, rsa_private_key)
+
+        api("/control-cluster/switch-certificate", :put,
+        {:certificate => {:pem_encoded => pem_encoded}, :rsa_private_key => rsa_private_key})
+
+    end
+
+    private
+
+    def https(uri)
+
+        https = Net::HTTP.new(uri.host, uri.port)
+        https.use_ssl = true
+        https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        https
+
+    end
+
+    def api(path, method, body={})
+
+        uri = URI.parse("#{@prefix}#{path}")
